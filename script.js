e2f0ef81e690d7d59307e3b5a627d7c587920f99
Christof Kälin <christof.kaelin@pm.me>
2024-01-12 18:58:39 +0100

    coding challenge

--- script.js
+++ script.js
@@ -15,4 +15,13 @@ const measureKelvin = function () {
   return kelvin
 }
 
-console.log(measureKelvin())
+function printForecast(temps) {
+  let output = ""
+  for (let i = 0; i < temps.length; i++) {
+    output = output.concat(`... ${temps[i]}°C in ${i + 1} days`)
+  }
+  console.log(output)
+}
+
+printForecast([17, 21, 23])
+printForecast([12, 5, -5, 0, 4])
