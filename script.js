6138180a605d5f23ab2ea0a90c634022be951259
Christof Kälin <christof.kaelin@pm.me>
2024-01-12 18:37:07 +0100

    debugging practice

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-"use strict"
 nums = new Array(100, 300, 304, 304)
 console.log(nums)
 nums[0] = 999
 console.log(nums)
+
+console.log("Hello")
+
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    value: Number(prompt("Degree celsius")),
+  }
+  const kelvin = measurement.value + 273
+  return kelvin
+}
+
+console.log(measureKelvin())
