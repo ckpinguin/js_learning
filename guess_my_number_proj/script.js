635782b14f92175f65a82945b306508d733be12c
Christof Kälin <christof.kaelin@pm.me>
2024-01-17 22:48:57 +0100

    some game logic

--- guess_my_number_proj/script.js
+++ guess_my_number_proj/script.js
@@ -1,15 +1,25 @@
 "use strict"
 
-const secretNumber = 12
+const secretNumber = Math.trunc(Math.random() * 20) + 1
+let score = 20
+document.querySelector(".number").textContent = secretNumber
+
+const scoreEl = document.querySelector(".score")
 const checkBtn = document.querySelector(".check")
 const message = document.querySelector(".message")
 checkBtn.addEventListener("click", () => {
   const guess = Number(document.querySelector(".guess").value)
   if (!guess) {
-    alert("No number guessed!")
+    message.textContent = "⛔️ No number!"
   } else if (guess === secretNumber) {
     message.textContent = "🏆🏆🏆 Correct!"
-  } else {
-    message.textContent = "👎 Nope"
+  } else if (guess > secretNumber) {
+    message.textContent = "📈 Too high!"
+    score--
+    scoreEl.textContent = score
+  } else if (guess < secretNumber) {
+    message.textContent = "📉 Too low!"
+    score--
+    scoreEl.textContent = score
   }
 })
