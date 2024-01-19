e832928a05d9b24cc59e9021fd311925e9929cda
Christof Kälin <christof.kaelin@pm.me>
2024-01-19 09:15:17 +0100

    cleaner code

--- guess_my_number_proj/script.js
+++ guess_my_number_proj/script.js
@@ -1,8 +1,7 @@
 "use strict"
 
-const scoreEl = document.querySelector(".score")
 const checkBtn = document.querySelector(".check")
-const message = document.querySelector(".message")
+
 const againBtn = document.querySelector(".again")
 const guessNum = document.querySelector(".guess")
 const num = document.querySelector(".number")
@@ -12,51 +11,62 @@ const hiScoreEl = document.querySelector(".highscore")
 let score = 20
 let secretNumber = 0
 let hiScore = 0
-
+againBtn.disabled = true
 resetGame()
-
-checkBtn.addEventListener("click", () => {
-  const guess = Number(guessNum.value)
-  if (score > 0) {
-    if (!guess) {
-      message.textContent = "⛔️ No number!"
-    } else if (guess === secretNumber) {
-      message.textContent = "🏆🏆🏆 Correct!"
-      body.style.backgroundColor = "#60b347"
-      guessNum.style.width = "30rem"
-      if (score > hiScore) {
-        hiScore = score
-        hiScoreEl.textContent = hiScore
-      }
-    } else if (guess > secretNumber) {
-      message.textContent = "📈 Too high!"
-      score--
-      console.log(score)
-      scoreEl.textContent = score
-    } else if (guess < secretNumber) {
-      message.textContent = "📉 Too low!"
-      score--
-      console.log(score)
-      scoreEl.textContent = score
-    }
-  } else {
-    message.textContent = "👎 You lose!"
-    num.textContent = secretNumber
-    num.style.backgroundColor = "red"
-  }
-})
 
 function resetGame() {
   score = 20
-  scoreEl.textContent = score
+  updateScoreDisplay(score)
   secretNumber = Math.trunc(Math.random() * 20) + 1
-  message.textContent = "Start guessing..."
+  displayMessage("Start guessing...")
   guessNum.value = null
   num.textContent = "?"
   num.style.background = "#eee"
   document.querySelector("body").style.backgroundColor = "#222"
+  againBtn.disabled = true
+}
+
+function updateScoreDisplay(score) {
+  const scoreEl = document.querySelector(".score")
+  scoreEl.textContent = score
+}
+
+function displayMessage(content) {
+  const message = document.querySelector(".message")
+  message.textContent = content
 }
 
 againBtn.addEventListener("click", () => {
   resetGame()
+})
+
+checkBtn.addEventListener("click", () => {
+  const guess = Number(guessNum.value)
+  if (!guess) {
+    displayMessage("⛔️ No number!")
+  } else if (guess === secretNumber) {
+    displayMessage("🏆🏆🏆 Correct!")
+    body.style.backgroundColor = "#60b347"
+    guessNum.style.width = "30rem"
+    if (score > hiScore) {
+      hiScore = score
+      hiScoreEl.textContent = hiScore
+    }
+    againBtn.disabled = false
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!")
+      score--
+      console.log(score)
+    } else {
+      displayMessage("👎 You lose!")
+      num.textContent = secretNumber
+      num.style.backgroundColor = "red"
+      againBtn.addEventListener("click", () => {
+        resetGame()
+      })
+      againBtn.disabled = false
+    }
+  }
+  updateScoreDisplay(score)
 })
