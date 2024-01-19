9ea177373bc41af8e2a229e82e8a131e01818b31
Christof Kälin <christof.kaelin@pm.me>
2024-01-19 21:30:24 +0100

    Add current score functionality to roll dice button click event

--- pig_game/script.js
+++ pig_game/script.js
@@ -6,16 +6,28 @@ const btnRoll = document.querySelector(".btn--roll")
 const btnHold = document.querySelector(".btn--hold")
 const score0El = document.getElementById("score--0")
 const score1El = document.getElementById("score--1")
+const current0El = document.getElementById("current--0")
+const current1El = document.getElementById("current--1")
 
 score0El.textContent = 0
 score1El.textContent = 1
 diceEl.classList.add("hidden")
+
+let currentScore = 0
 
 btnRoll.addEventListener("click", () => {
   const dice = rollDice()
   console.log(dice)
   diceEl.classList.remove("hidden")
   diceEl.src = `dice-${dice}.png`
+
+  if (dice !== 1) {
+    currentScore += dice
+    current0El.textContent = currentScore
+  } else {
+    currentScore = 0
+    current0El.textContent = currentScore
+  }
 })
 
 function rollDice() {
