2f0503600860b94359be97a109aa7bf3861b96bb
Christof Kälin <christof.kaelin@pm.me>
2024-02-06 19:53:35 +0100

    Code challenge 2

--- data_structures_modern_operators_etc/script.js
+++ data_structures_modern_operators_etc/script.js
@@ -453,3 +453,57 @@ console.log(entries)
 
 const entries2 = Object.entries(books[0].thirdParty.goodreads)
 console.log(entries2)
+
+//
+
+/////////////////////////////////////////////
+// Coding Challenge #2
+/*
+Let's continue with our football betting app! Keep using the 'game' variable from before.
+Your tasks:
+1. Loop over the game.scored array and print each playername to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console,but in a nice formatted way,exactly like this:
+Odd of victory Bayern Munich: 1.33 Odd of draw: 3.25
+Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). Hint: Note how the odds and the game objects have the same property names😉
+4. Bonus:Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+     {
+       Gnarby: 1,
+       Hummels: 1,
+       Lewandowski: 2
+}
+GOOD LUCK😀
+*/
+// 1.
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${player}`)
+}
+
+// 2.
+const odds = Object.values(game.odds)
+let avg = 0
+for (const odd of odds) {
+  avg += odd
+}
+avg /= odds.length
+console.log(avg)
+
+// 3.
+for (const odd of Object.entries(game.odds)) {
+  if (game[odd[0]] == "x") {
+    console.log(`Odd of draw:${odd[1]}`)
+  }
+  console.log(`Odd of victory ${game[odd[0]]}: ${odd[1]}`)
+}
+
+// 4.
+const scorers = {}
+game.scored.forEach((player) => {
+  if (scorers[player]) {
+    scorers[player]++
+  } else {
+    scorers[player] = 1
+  }
+})
+console.log(scorers)
