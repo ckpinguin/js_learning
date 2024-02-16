45c6cf77badd810f9eceae58d0993bc0f71927fb
Christof Kälin <christof.kaelin@pm.me>
2024-02-16 11:04:24 +0100

    update

--- functions/script.js
+++ functions/script.js
@@ -29,9 +29,9 @@ const checkIn = function (flightNum, passenger) {
   passenger.name = "Mr. " + passenger.name
 
   if (passenger.passport === 24739479284) {
-    alert("Checked in")
+    console.log("Checked in")
   } else {
-    alert("Wrong passport!")
+    console.log("Wrong passport!")
   }
 }
 checkIn(flight, jonas)
