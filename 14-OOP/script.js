2110b4389777545b49b6482ec4141ea5817510da
Christof Kälin <christof.kaelin@protonmail.com>
2024-06-15 13:18:55 +0200

    class syntax

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -115,3 +115,31 @@ bmw.brake()
 mercedes.accelerate()
 mercedes.brake()
 mercedes.brake()
+
+// class expression
+// const PersonCl = class {}
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName
+    this.birthYear = birthYear
+  }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear)
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`)
+  }
+}
+
+const jessica = new PersonCl("Jessica", 1996)
+console.log(jessica)
+console.log(jessica.__proto__ === PersonCl.prototype)
+
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class citizens
+// 3. Classes are implicitly executed in strict mode
