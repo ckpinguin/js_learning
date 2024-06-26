5a63e9d6ea38ea70f1393550949571058fd9d83f
Christof Kälin <christof.kaelin@protonmail.com>
2024-06-26 14:37:53 +0200

    chore: Update Mapty app with Leaflet library for map functionality

--- 15-Mapty-OOP-App/script.js
+++ 15-Mapty-OOP-App/script.js
@@ -10,3 +10,25 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+      // for satellite images:
+      //https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png
+
+      L.tileLayer('https://tile.osm.ch/switzerland/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords).addTo(map).bindPopup('You worked out here').openPopup();
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
