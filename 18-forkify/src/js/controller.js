0a66ef91766dfd6026fa9cfb2e9f62ecd9179c73
Christof Kälin <christof.kaelin@protonmail.com>
2024-06-28 16:11:59 +0200

    first tries

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -15,11 +15,27 @@ const timeout = function (s) {
 const showRecipe = async function () {
   try {
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886aaa'
+      //'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      'https://forkify-api.herokuapp.com/api/v2/recipes/664c8f193e7aa067e94e8534'
     );
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (HTTP ${res.status})`);
+
+    console.log(res, data);
+    let { recipe } = data.data;
+
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
   } catch (err) {
     alert(err);
   }
